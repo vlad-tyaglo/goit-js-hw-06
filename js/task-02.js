f23098c21c8841bccca3a776a8d093753988d16e
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2022-12-18 21:24:16 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let listArr = [];
+for (const ingredient of ingredients){
+    const listEl = document.createElement('li');
+    listEl.textContent = ingredient;
+    listEl.classList.add('item');
+    listArr.push(listEl);
+};
+const ulEl = document.querySelector('#ingredients')
+ulEl.append(...listArr);
