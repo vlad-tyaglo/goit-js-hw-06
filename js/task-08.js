348057f7c52a2079b9e0616e13a936e446248adc
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-14 00:42:58 +0200

    upd task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
+const inputName = document.querySelector('input[name="email"]');
+const inputPassword = document.querySelector('input[name="password"]');
+form.addEventListener("submit", sendSubmit);
+
+function sendSubmit(event) {
+    event.preventDefault();
+    console.dir(form)
+    const formData =  new FormData(event.currentTarget);
+
+    console.log(formData);
+  
+    formData.forEach((value, name) => {
+    console.log(`name`, name );
+    console.log(`value`, value);
+    });
+
+    
+    if (inputName.value === '' || inputPassword.value === ''){
+       alert("Всі поля повинні бути заповнені")
+    }
+
+    form.reset();
+
+};
+
 
