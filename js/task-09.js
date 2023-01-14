33329b09209173e6c96358de215e59fb0fcc3f59
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-14 21:41:58 +0200

    update 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.attributes.style.value;
+  console.dir(bodyEl);
+}
